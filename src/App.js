14d5e8937c33baa8a7589879c1a6436510d9fc14
Tomas Yanez <tyanez05@gmail.com>
2020-06-29 23:19:52 -0400

    Base components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import './App.css';
 
-function App() {
+import MonumentsMap from './components/map/MonumentsMap';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+	  	<Grid item xs={12}>
+        	<Paper className={classes.paper}>
+				<Typography variant="h3" gutterBottom>
+					Titulo
+				</Typography>
+			</Paper>
+        </Grid>
+		<Grid item xs={12}>
+        	<Paper className={classes.paper}>
+				<MonumentsMap />
+			</Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
